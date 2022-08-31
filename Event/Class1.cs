7baa6668c62a531bc1fa415060b19eaa3e33a9f7
aayangunna <88987151+aayangunna@users.noreply.github.com>
2022-08-31 15:13:52 +0100

    Add project files.

---
+++ Event/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventAssignment
+{
+    /// <summary>
+    /// This is the publisher class
+    /// </summary>
+    internal class AccessControl
+    {
+        public delegate void DoorSensorHandler();
+        public event DoorSensorHandler Sensor;
+
+        /// <summary>
+        /// Detects if there is any movement
+        /// </summary>
+        public void MoveDetected()
+        {
+            Console.WriteLine("Detecting Movement");
+            Thread.Sleep(5000);
+            OnMoveDetected();
+        }
+
+        /// <summary>
+        /// This method raises event on detecting movement
+        /// </summary>
+        public virtual void OnMoveDetected()
+        {
+            Sensor?.Invoke();
+        }
+    }
+
+    /// <summary>
+    /// Subscriber class that subscribes to 
+    /// the AccessControl class
+    /// </summary>
+    public class CentralControl
+    {
+
+        public void GrantDet()
+        {
+            var access = new AccessControl();
+            access.Sensor += OpenDoor;
+            access.MoveDetected();
+        }
+        public void OpenDoor()
+        {
+            Console.WriteLine("A staff is approaching open the door");
+        }
+    }
+}
---
+++ Event/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EventAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var secSys = new CentralControl();
+            secSys.GrantDet();
+        }
+    }
+}
